5ef957da1152b7c056c55e37055dd8642430e772
mateusselhorst <mateus_selhorst@estudante.sesisenai.org.br>
2025-03-19 10:12:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// este é o primeiro exercício
+function verificarPar(numero) {
+    if (numero % 2 === 0) {
+        console.log("Este numero é par");
+    } else {
+        console.log("Este numero é ímpar");
+    }
+}
+
+verificarPar(0);
+verificarPar(9);
+verificarPar(0);
+
+// este é o segundo exercício
+
+function calcularNumero(numero1,numero2) {
+ console.log(numero1 + numero2);
+ console.log(numero1 - numero2);
+ console.log(numero1 * numero2);
+ console.log(numero2 !== 0 ? numero1 / numero2: 'Não pode ser divisível por 0');
+}
+
+calcularNumero (10,0)
+
+//3º questão
+
+    for (let i = 10; i > 0; i--){
+    console.log (i)
+}
+
